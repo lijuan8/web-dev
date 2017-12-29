4d9aa51bcb81e10be5ff952cda4015aa7cabf8bf
Lijuan Yang <lijuan.yang@searshc.com>
2017-12-28 20:29:10 -0800

    initial

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
+var path= require('path');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 require ("./test/app.js")(app);
 
-var port = process.env.PORT || 3000;
+app.set('port', (process.env.PORT || 3000));
 
-app.listen(port);
+app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'));
+});
